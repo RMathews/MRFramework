ec94ef84523792c29461dc0b3b171d42bf02f237
Revon Mathews <revon.mathews@intel.com>
2016-10-10 14:07:00 -0500

    added remaining

---
+++ src/org/hashbang/jobtracker/IJobTracker.java
@@ -0,0 +1,21 @@
+package org.hashbang.jobtracker;
+
+
+public interface IJobTracker {
+
+    /**
+     * Parses the commandline arguments
+     */
+    public void parseArgs(String[] args);
+
+    /**
+     * Creates a {@link org.hashbang.mr.JobContext} for the submitted job
+     */
+    public void createJobContext();
+
+    /**
+     * Submits map or reduce tasks to the task trackers.
+     */
+    public void submitTask();
+
+}
